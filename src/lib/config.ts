4c815c3a5bad4cf01ea2be7831a1c899658b602f
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2026-04-19 17:42:51 +0700

    Refactor apiBaseUrl logic for environment variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.NODE_ENV === "production" ? "https://e3-backend.vercel.app" : (process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000");
+export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === "production" ? "https://e3-be.vercel.app" : "http://localhost:5000");
